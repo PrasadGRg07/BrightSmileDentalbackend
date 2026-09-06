aaeaf403eebcad4c25c3f72ba524c83c9dadac30
Prasad Gurung <prasadgurung506@gmail.com>
2026-09-06 22:59:53 +0545

    updated buildsh

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
+  python manage.py createsuperuser \
+    --noinput \
+    --username "${DJANGO_SUPERUSER_USERNAME}" \
+    --email "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}" || true
+fi
